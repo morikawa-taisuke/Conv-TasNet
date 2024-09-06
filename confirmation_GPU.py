26b9f47c4790b0d85b2456504b2ed0646f56c45f
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-09-06 16:39:29 +0900

    confirmation_GPU.pyの削除

--- confirmation_GPU.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print('torch version:', torch.__version__)  # torchのバージョン
-print('GPU availability:', torch.cuda.is_available())  # GPUが使える場合True, 使えない場合False
-print('Number of GPUs available in PyTorch:', torch.cuda.device_count())  # 使えるGPUの個数?
-print('GPU name:', torch.cuda.get_device_name())  # PCに搭載されているGPUの名前
