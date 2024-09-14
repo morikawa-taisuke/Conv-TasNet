270296078a840261731836e92b07aae22a514383
sub_desktop <y24m019@mail.ryukoku.ac.jp>
2024-09-15 00:16:48 +0900

    subPC 4ch_circular_10cm test_eval

--- test_evaluation.py
+++ test_evaluation.py
@@ -3,20 +3,20 @@ import Multi_Channel_ConvTasNet_test as test
 from mymodule import const
 
 
-angle_list = ['Right', 'FrontRight', 'Front', 'FrontLeft', 'Left']  # 'Right', 'FrontRight', 'Front', 'FrontLeft', 'Left'
+angle_list = ['FrontRight', 'Front', 'FrontLeft', 'Left']  # 'Right', 'FrontRight', 'Front', 'FrontLeft', 'Left'
 
 condition = {"speech_type": 'subset_DEMAND',
              "noise": 'hoth',
              "snr": 10,
              "reverbe": 5}
 # ch_list = [2, 4]
-ch = 2
+ch = 4
 # for ch in ch_list:
 for angle in angle_list:
-    dir_name = f'sebset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm_Dtype'
+    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm_Dtype'
     out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}'
-    wave_path = f"{const.MIX_DATA_DIR}\\sebset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm\\{angle}\\test\\"
-    wave_type_list = ['noise_only']    # 'noise_only', 'noise_reverbe', 'reverbe_only'
+    wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm\\{angle}\\test\\"
+    wave_type_list = ['noise_reverbe', 'reverbe_only']    # 'noise_only', 'noise_reverbe', 'reverbe_only'
     for wave_type in wave_type_list:
         print('test')
         test.test(mix_dir=f'{wave_path}/{wave_type}',
@@ -26,9 +26,9 @@ for angle in angle_list:
                   model_type='D')
 
         print('evaluation')
-        eval.main(target_dir=f"{const.MIX_DATA_DIR}\\sebset_DEMAND_hoth_1010dB_05sec_4ch_3cm\\{angle}\\test\\clean",
+        eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm\\{angle}\\test\\clean",
                   estimation_dir=f'{out_dir}/{wave_type}',
-                  out_path=f'{const.EVALUATION_DIR}\\sebset_DEMAND_hoth_1010dB_05sec_4ch_3cm\\{angle}\\{wave_type}.csv',
+                  out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm\\{angle}\\{wave_type}.csv',
                   condition=condition,
                   channel=ch)
 
