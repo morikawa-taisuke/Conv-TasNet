9c1c6e3a18721d3907a222259a722713a647e2b7
sub_desktop <y24m019@mail.ryukoku.ac.jp>
2024-09-15 15:09:27 +0900

    subPC 2ch_3cm 2ch_6cm 2ch_10cm makedataset

--- make_dataset.py
+++ make_dataset.py
@@ -581,7 +581,7 @@ def process_dataset_thread(wav_type, ch, angle):
     # C:\\Users\\kataoka-lab\\Desktop\\sound_data\\mix_data\\sebset_DEMAND_hoth_1010dB_05sec_4ch_3cm\\Back\\train\\noise_reverbe
     # angle = 'Front'
     # subset_DEMAND_hoth_1010dB_05sec_4ch_circular_10cm
-    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm'
+    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm'
     mix_dir = f'{const.MIX_DATA_DIR}\\{dir_name}\\{angle}\\train\\{wav_type}'
     target_dir = f'{const.MIX_DATA_DIR}\\{dir_name}\\{angle}\\train\\clean'
     out_dir = f'{const.DATASET_DIR}\\{dir_name}\\{angle}\\{wav_type}'
@@ -627,7 +627,7 @@ if __name__ == '__main__':
     #                       num_mic=4)
 
     wav_type_list = ['noise_only', 'noise_reverbe', 'reverbe_only']
-    ch_list = 4
+    ch_list = 2
     angle_name_list = ['Right', 'FrontRight', 'Front', 'FrontLeft', 'Left']
 
     start = time.time()
