56795694f1b016311f8a6d4836407a6352fffb5a
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-09-18 10:10:18 +0900

    mainPC 4ch_3cm test_eval

--- test_evaluation.py
+++ test_evaluation.py
@@ -10,10 +10,10 @@ condition = {"speech_type": 'subset_DEMAND',
              "snr": 10,
              "reverbe": 5}
 # ch_list = [2, 4]
-ch = 2
+ch = 4
 # for ch in ch_list:
 for angle in angle_list:
-    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_2ch_3cm_Dtype'
+    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_4ch_3cm_Dtype'
     out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}'
     wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm\\{angle}\\test\\"
     wave_type_list = ['noise_reverbe', 'reverbe_only']    # 'noise_only', 'noise_reverbe', 'reverbe_only'
