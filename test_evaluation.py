fd513375501bbd2a9b0373dfa850593a9b39c718
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-09-18 10:22:16 +0900

    mainPC 4ch_circular_10cm_45C test_eval

--- test_evaluation.py
+++ test_evaluation.py
@@ -13,9 +13,9 @@ condition = {"speech_type": 'subset_DEMAND',
 ch = 4
 # for ch in ch_list:
 for angle in angle_list:
-    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_4ch_3cm_Dtype'
+    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_4ch_circular_10cm_45C_Dtype'
     out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}'
-    wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm\\{angle}\\test\\"
+    wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm_45C\\{angle}\\test\\"
     wave_type_list = ['noise_reverbe', 'reverbe_only']    # 'noise_only', 'noise_reverbe', 'reverbe_only'
     for wave_type in wave_type_list:
         print('test')
@@ -26,9 +26,9 @@ for angle in angle_list:
                   model_type='D')
 
         print('evaluation')
-        eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm\\{angle}\\test\\clean",
+        eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm_45C\\{angle}\\test\\clean",
                   estimation_dir=f'{out_dir}/{wave_type}',
-                  out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_3cm\\{angle}\\{wave_type}.csv',
+                  out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_10cm_45C\\{angle}\\{wave_type}.csv',
                   condition=condition,
                   channel=ch)
 
