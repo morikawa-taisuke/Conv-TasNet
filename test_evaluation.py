fc682f4278e4ab6fd3f376d8c8b9f1ea9c1d6310
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-09-27 13:21:03 +0900

    mainPC 4cm_6cm_circular_45C test_eval

--- test_evaluation.py
+++ test_evaluation.py
@@ -9,41 +9,41 @@ condition = {"speech_type": 'subset_DEMAND',
              "noise": 'hoth',
              "snr": 10,
              "reverbe": 5}
-# ch = [2, 4]\subset_DEMAND_hoth_1010dB_05sec_4ch_10cm
-ch = 2
-distance = 3
+# ch = [2, 4]\
+ch = 4
+distance = 6
 # for ch in ch:
-# for angle in angle_list:
-dir_name = f'subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm'
-out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/'
-# out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}'
-wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\test\\"
-# wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm\\{angle}\\test\\"
-wave_type_list = ['noise_only', 'reverbe_only', 'noise_reverbe']    # 'noise_only', 'reverbe_only', 'noise_reverbe'
-for wave_type in wave_type_list:
-    print('test')
-    # test.test(mix_dir=f'{wave_path}/{wave_type}',
-    #           out_dir=f'{out_dir}/{wave_type}',
-    #           model_name=f'{const.PTH_DIR}/{dir_name}/{angle}/{wave_type}/{wave_type}_100.pth',
-    #           channels=ch,
-    #           model_type='D')
+for angle in angle_list:
+    dir_name = f'subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_Dtype'
+    # out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/'
+    out_dir = f'{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}'
+    # wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\test\\"
+    wave_path = f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_circular_{distance}cm_45C\\{angle}\\test\\"
+    wave_type_list = ['noise_only', 'reverbe_only', 'noise_reverbe']    # 'noise_only', 'reverbe_only', 'noise_reverbe'
+    for wave_type in wave_type_list:
+        print('test')
+        test.test(mix_dir=f'{wave_path}/{wave_type}',
+                  out_dir=f'{out_dir}/{wave_type}',
+                  model_name=f'{const.PTH_DIR}/{dir_name}/{angle}/{wave_type}/{wave_type}_100.pth',
+                  channels=ch,
+                  model_type='D')
 
-    test.test(mix_dir=f'{wave_path}/{wave_type}',
-              out_dir=f'{out_dir}/{wave_type}',
-              model_name=f'{const.PTH_DIR}/{dir_name}/{wave_type}/{wave_type}_100.pth',
-              channels=ch,
-              model_type='D')
+        # test.test(mix_dir=f'{wave_path}/{wave_type}',
+        #           out_dir=f'{out_dir}/{wave_type}',
+        #           model_name=f'{const.PTH_DIR}/{dir_name}/{wave_type}/{wave_type}_100.pth',
+        #           channels=ch,
+        #           model_type='D')
 
-    print('evaluation')
-    # eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm\\{angle}\\test\\clean",
-    #           estimation_dir=f'{out_dir}/{wave_type}',
-    #           out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm\\{angle}\\{wave_type}.csv',
-    #           condition=condition,
-    #           channel=ch)
+        print('evaluation')
+        eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm\\{angle}\\test\\clean",
+                  estimation_dir=f'{out_dir}/{wave_type}',
+                  out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm\\{angle}\\{wave_type}.csv',
+                  condition=condition,
+                  channel=ch)
 
-    eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\test\\clean",
-              estimation_dir=f'{out_dir}/{wave_type}',
-              out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\{wave_type}.csv',
-              condition=condition,
-              channel=ch)
+        # eval.main(target_dir=f"{const.MIX_DATA_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\test\\clean",
+        #           estimation_dir=f'{out_dir}/{wave_type}',
+        #           out_path=f'{const.EVALUATION_DIR}\\subset_DEMAND_hoth_1010dB_05sec_{ch}ch_{distance}cm_all_angle\\{wave_type}.csv',
+        #           condition=condition,
+        #           channel=ch)
 
