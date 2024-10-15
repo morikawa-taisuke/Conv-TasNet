8383052774ba9148714198c2c6785e9a5a2d8bce
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-10-15 21:36:58 +0900

    mainPC 1ch_to_4ch_win_array test

--- test_evaluation.py
+++ test_evaluation.py
@@ -16,25 +16,25 @@ ch = 4
 # distance = 6
 # for ch in ch:
 
-# for reverbe in range(1, 6):
-# C:\Users\kataoka-lab\Desktop\sound_data\mix_data\subset_DEMAND_hoth_1010dB_05sec_4ch_circular_6cm_45C
 # dir_name = "subset_DEMAND_hoth_1010dB_05sec_4ch_circular_6cm"
-dir_name = "subset_DEMAND_hoth_1010dB_05sec_4ch_circular_6cm"
-wave_type_list = ["reverbe_only", "noise_reverbe"]    # "noise_only", "reverbe_only", "noise_reverbe"
+dir_name = "subset_DEMAND_hoth_1010dB_1ch_to_4ch_win_array"
+wave_type_list = ["reverbe_only"]    # "noise_only", "reverbe_only", "noise_reverbe"
 for wave_type in wave_type_list:
-    for angle in angle_list:
-        print("test")
-        test.test(mix_dir=f"{const.MIX_DATA_DIR}/{dir_name}/{angle}/test/{wave_type}",
-                  out_dir=f"{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}/{wave_type}",
-                  model_name=f"{const.PTH_DIR}/{dir_name}/{wave_type}_100.pth",
-                  channels=ch,
-                  model_type="D")
+    # for reverbe in range(1, 6):
+    reverbe = 4
+    # for angle in angle_list:
+    print("test")
+    test.test(mix_dir=f"{const.MIX_DATA_DIR}/{dir_name}/{reverbe:02}sec/test/{wave_type}",
+              out_dir=f"{const.OUTPUT_WAV_DIR}/{dir_name}/{reverbe:02}sec/{wave_type}",
+              model_name=f"{const.PTH_DIR}/{dir_name}/{reverbe:02}sec/{dir_name}_{wave_type}/{dir_name}_{wave_type}_100.pth",
+              channels=ch,
+              model_type="D")
 
 
-        print("evaluation")
-        eval.main(target_dir=f"{const.MIX_DATA_DIR}/{dir_name}/{angle}/test//clean",
-                  estimation_dir=f"{const.OUTPUT_WAV_DIR}/{dir_name}/{angle}/{wave_type}",
-                  out_path=f"{const.EVALUATION_DIR}\\{dir_name}\\{angle}\\{wave_type}.csv",
-                  condition=condition,
-                  channel=ch)
+    print("evaluation")
+    eval.main(target_dir=f"{const.MIX_DATA_DIR}/{dir_name}/{reverbe:02}sec/test/clean",
+              estimation_dir=f"{const.OUTPUT_WAV_DIR}/{dir_name}/{reverbe:02}sec/{wave_type}",
+              out_path=f"{const.EVALUATION_DIR}\\{dir_name}\\{reverbe:02}sec\\{wave_type}.csv",
+              condition=condition,
+              channel=ch)
 
