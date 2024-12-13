cfddaca01bd4a675c72475be778941ee630fa4c5
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-12-13 22:54:11 +0900

    mainPC 1ch_to_4ch win_junnbann_minus train test_eval

--- main_1ch_to_4ch.py
+++ main_1ch_to_4ch.py
@@ -406,38 +406,38 @@ if __name__ == "__main__":
     print("start")
     """ ファイル名等の指定 """
     # C:\\Users\\kataoka-lab\\Desktop\\sound_data\\mix_data\\subset_DEMAND_hoth_1010dB_1ch\\05sec\\train\\
-    base_name = "subset_DEMAND_hoth_1010dB_1ch_to_4ch_win_array"
+    base_name = "subset_DEMAND_hoth_1010dB_1ch_to_4ch_win_junnban_minus"
     wave_type_list = ["noise_reverbe", "reverbe_only", "noise_only"]     # "noise_reverbe", "reverbe_only", "noise_only"
     # angle_list = ["Right", "FrontRight", "Front", "FrontLeft", "Left"]  # "Right", "FrontRight", "Front", "FrontLeft", "Left"
     channel = 4
     """ wav_fileの作成 """
     mix_dir = f"{const.MIX_DATA_DIR}/{base_name}/05sec"
     input_dir = f"{const.MIX_DATA_DIR}/subset_DEMAND_hoth_1010dB_1ch/subset_DEMAND_hoth_1010dB_05sec_1ch/"
-    # for test_train in my_func.get_subdir_list(input_dir):
-    #     for wave_type in my_func.get_subdir_list(os.path.join(input_dir, test_train)):
-    #         make_dataset.multi_to_single_wavfile(mix_dir=os.path.join(input_dir, test_train, wave_type),
-    #                                              out_dir=os.path.join(mix_dir, test_train, wave_type),
-    #                                              channel=channel)
+    for test_train in my_func.get_subdir_list(input_dir):
+        for wave_type in my_func.get_subdir_list(os.path.join(input_dir, test_train)):
+            make_dataset.multi_to_single_wavfile(mix_dir=os.path.join(input_dir, test_train, wave_type),
+                                                 out_dir=os.path.join(mix_dir, test_train, wave_type),
+                                                 channel=channel)
 
     """ datasetの作成 """
     print("make_dataset")
     dataset_dir = f"{const.DATASET_DIR}/{base_name}/05sec"
     mix_dir = f"{const.MIX_DATA_DIR}/{base_name}/05sec"
-    # for wave_type in wave_type_list:
-    #     # for angel in angle_list:
-    #     make_dataset.multi_channel_dataset2(mix_dir=os.path.join(mix_dir, "train", wave_type),
-    #                                         target_dir=os.path.join(mix_dir, "train", "clean"),
-    #                                         out_dir=os.path.join(dataset_dir, wave_type),
-    #                                         channel=channel)
+    for wave_type in wave_type_list:
+        # for angel in angle_list:
+        make_dataset.multi_channel_dataset2(mix_dir=os.path.join(mix_dir, "train", wave_type),
+                                            target_dir=os.path.join(mix_dir, "train", "clean"),
+                                            out_dir=os.path.join(dataset_dir, wave_type),
+                                            channel=channel)
     """ train """
     print("train")
     pth_dir = f"{const.PTH_DIR}/{base_name}/"
-    # for wave_type in wave_type_list:
-    #     main(dataset_path=os.path.join(dataset_dir, wave_type),
-    #          out_path=os.path.join(pth_dir,wave_type),
-    #          train_count=100,
-    #          model_type="D",
-    #          channel=channel)
+    for wave_type in wave_type_list:
+        main(dataset_path=os.path.join(dataset_dir, wave_type),
+             out_path=os.path.join(pth_dir,wave_type),
+             train_count=100,
+             model_type="D",
+             channel=channel)
 
     """ test_evaluation """
     condition = {"speech_type": "subset_DEMAND",
@@ -445,7 +445,7 @@ if __name__ == "__main__":
                  "snr": 10,
                  "reverbe": 5}
     for wave_type in wave_type_list:
-        name = "subset_DEMAND_hoth_1010dB_1ch_to_4ch_win_array/05sec"
+        name = f"{base_name}/05sec"
         mix_dir = f"{const.MIX_DATA_DIR}/{name}/test"
         out_wave_dir = f"{const.OUTPUT_WAV_DIR}/{base_name}/05sec/"
         print("test")
