c5ba9166e97024c96de7723da43fa49bb0147450
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2024-12-31 13:12:39 +0900

    mainPC コンフリクトの解消

--- nanotonak.py
+++ nanotonak.py
@@ -38,25 +38,44 @@ def main(wav_data, num_delay, c=340, SR=16000):
     out_wav = wav_data * decay
     return out_wav
 
+def scaling(wav_path, out_dir):
+    max_data = np.iinfo(np.int16).max
+
+    input_data, prm = my_func.load_wav(wav_path)
+    input_data = input_data/max(np.abs(input_data)) * max_data
+
+    file_name, _ = my_func.get_file_name(wav_path)
+    out_path = f"{out_dir}/{file_name}.wav"
+
+    my_func.save_wav(out_path, input_data, prm)
+
 
 
 if __name__ == "__main__":
     print("start")
-    input_path = "C:\\Users\\kataoka-lab\\Desktop\\sound_data\\mix_data\\subset_DEMAND_hoth_1010dB_1ch\\subset_DEMAND_hoth_1010dB_05sec_1ch\\test\\noise_reverbe\\p232_068_16kHz_hoth_10db_05sec_None.wav"
-    input_data, prm = my_func.load_wav(input_path)
-    input_data[input_data==0] = 1
-    out_wav = []
-    win = 32
-    out_wav.append(input_data)
-    for ch in range(1, 3+1):
-        decay = main(input_data, num_delay=win*ch)
-        out_wav.append(decay)
-    out_wav = np.array(out_wav)
-    print("out_wav",out_wav.shape)
-    out_path = "C:\\Users\\kataoka-lab\\Desktop\\sound_data\\decay.wav"
-    my_func.save_wav(out_path=out_path,
-                     wav_data=out_wav,
-                     prm=prm)
+    # input_path = "C:\\Users\\kataoka-lab\\Desktop\\sound_data\\mix_data\\subset_DEMAND_hoth_1010dB_1ch\\subset_DEMAND_hoth_1010dB_05sec_1ch\\test\\noise_reverbe\\p232_068_16kHz_hoth_10db_05sec_None.wav"
+    # input_data, prm = my_func.load_wav(input_path)
+    # input_data[input_data==0] = 1
+    # out_wav = []
+    # win = 32
+    # out_wav.append(input_data)
+    # for ch in range(1, 3+1):
+    #     decay = main(input_data, num_delay=win*ch)
+    #     out_wav.append(decay)
+    # out_wav = np.array(out_wav)
+    # print("out_wav",out_wav.shape)
+    # out_path = "C:\\Users\\kataoka-lab\\Desktop\\sound_data\\decay.wav"
+    # my_func.save_wav(out_path=out_path,
+    #                  wav_data=out_wav,
+    #                  prm=prm)
+
+    input_dir ="C:\\Users\\kataoka-lab\\Desktop\\AIと声と雑音と"
+    output_dir ="C:\\Users\\kataoka-lab\\Desktop\\AIと声と雑音と\\out"
+    wav_list = my_func.get_file_list(input_dir)
+
+    for wav_path in tqdm(wav_list):
+        scaling(wav_path, output_dir)
+
 
     # a = np.array([0, 10, 2, 3, 0, 0, 0])
     # print(a)
